7e73e42663bf6d053fe6e71a6e1b128b1244fdf6
Mark Harris <markcharris81@gmail.com>
2021-12-12 20:55:00 -0500

    Modified metrtictools script for this project.  Generated and saved predictions from CNN so classification metrics could be calculated.  Cleaned up notebook and added comments and markdown.

--- code/metrictools_snow.py
+++ code/metrictools_snow.py
@@ -6,19 +6,16 @@ from sklearn.metrics import classification_report, roc_auc_score, RocCurveDispla
 
 ############# SHORTCUTS FOR VERY COMMON GRAPHS ##############
 
-def check_metrics(estimator, X_train, X_test, y_train, y_test):
-    
-    train_preds = estimator.predict(X_train)
-    test_preds = estimator.predict(X_test)
+def check_metrics(train_preds, test_preds, y_train, y_test, cat_0, cat_1):
     
     print('Train Data Metrics:\n')
-    print(classification_report(y_train, train_preds, target_names=['Futurology', 'Science'], digits=4))
+    print(classification_report(y_train, train_preds, target_names=[cat_0, cat_1], digits=4))
     print(f'ROC AUC score: {roc_auc_score(y_train, train_preds)}')
     
     print('\n*************************\n')
 
     print('Test Data Metrics:\n')
-    print(classification_report(y_test, test_preds, target_names=['Futurology', 'Science'], digits=4))
+    print(classification_report(y_test, test_preds, target_names=[cat_0, cat_1], digits=4))
     print(f'ROC AUC score: {roc_auc_score(y_test, test_preds)}')
     
     print('\n*************************\n')
@@ -28,11 +25,11 @@ def check_metrics(estimator, X_train, X_test, y_train, y_test):
     RocCurveDisplay.from_predictions(y_train, train_preds, ax=axs[0,0])
     axs[0,0].set_title('Train Data ROC Curve', fontsize='x-large')
     
-    ConfusionMatrixDisplay.from_predictions(y_train, train_preds, display_labels=['Futurology', 'Science'], ax=axs[0,1])
+    ConfusionMatrixDisplay.from_predictions(y_train, train_preds, display_labels=[cat_0, cat_1], ax=axs[0,1])
     axs[0,1].set_title('Train Data Confusion Matrix', fontsize='x-large')
     
     RocCurveDisplay.from_predictions(y_test, test_preds, ax=axs[1,0])
     axs[1,0].set_title('Test Data ROC Curve', fontsize='x-large')
 
-    ConfusionMatrixDisplay.from_predictions(y_test, test_preds, display_labels=['Futurology', 'Science'], ax=axs[1,1])
+    ConfusionMatrixDisplay.from_predictions(y_test, test_preds, display_labels=[cat_0, cat_1], ax=axs[1,1])
     axs[1,1].set_title('Test Data Confustion Matrix', fontsize='x-large')
